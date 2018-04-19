ca7fb95639c02e86ef27b62d72d2e78b8da68a52
Michael A. Holmes <holmes.m.andrew@gmail.com>
2018-04-18 20:56:01 -0500

    First push for basic App setting up

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+//Define DB params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "shareboard");  //name whatever I want
+
+//Define URL
+define("ROOT_PATH", "/");
+define("ROOT_URL", "http:localhost:82/project.dev");
